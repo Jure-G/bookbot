294455b91e75ef6a59ae391fb5dfcddac139db35
Jure-G <gorjup.jure91@gmail.com>
2025-09-29 21:11:20 +0200

    fix count_characters in stats.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from stats import get_num_words
 from stats import count_characters
 
 def main():
+    print (get_num_words(get_book_text("books/frankenstein.txt")))
     print (count_characters(get_book_text("books/frankenstein.txt")))
-
+  
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
--- stats.py
+++ stats.py
@@ -3,14 +3,13 @@ def get_num_words(book_text):
     return len(list_of_words)
 
 def count_characters(book_text):
-    lowered_text_list = list(book_text.lower())
-    character_count_dict = []
-    for i in lowered_text_list:
-        if i is in character_count_dict:
-            character_count_dict[i]
-            pass
-    
-        
+    character_count_dict = {}
+    for i in book_text.lower():
+        if i not in character_count_dict:
+            character_count_dict[i] = 1
+        else:
+            character_count_dict[i] += 1
+
     return character_count_dict
         
         
