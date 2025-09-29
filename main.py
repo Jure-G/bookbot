7518ace69f2fc44d295936c1b05b4e2b04d91653
Jure-G <gorjup.jure91@gmail.com>
2025-09-29 20:02:25 +0200

    add main.py and stats.py, half way done with projext

---
+++ main.py
@@ -0,0 +1,14 @@
+from stats import get_num_words
+from stats import count_characters
+
+def main():
+    print (count_characters(get_book_text("books/frankenstein.txt")))
+
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+    
+main()
---
+++ stats.py
@@ -0,0 +1,16 @@
+def get_num_words(book_text):
+    list_of_words = book_text.split()
+    return len(list_of_words)
+
+def count_characters(book_text):
+    lowered_text_list = list(book_text.lower())
+    character_count_dict = []
+    for i in lowered_text_list:
+        if i is in character_count_dict:
+            character_count_dict[i]
+            pass
+    
+        
+    return character_count_dict
+        
+        
