875dcc4a61c8b62e9f74999a4b9306c56fb2a272
Jure-G <gorjup.jure91@gmail.com>
2025-09-30 21:56:45 +0200

    a lot of shit, finished tho :D

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
+import sys
 from stats import get_num_words
 from stats import count_characters
+from stats import get_book_text
+from stats import count_characters
+from stats import make_list_of_dictionaries
+from stats import sort_helper
+from stats import sort_list_of_dictionaries
 
 def main():
-    print (get_num_words(get_book_text("books/frankenstein.txt")))
-    print (count_characters(get_book_text("books/frankenstein.txt")))
-  
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_file = sys.argv[1]
+    text = get_book_text(path_to_file)
+    list_of_characters = count_characters(text)
+    list_of_dict = make_list_of_dictionaries(list_of_characters)
+    sorted_list = sort_list_of_dictionaries(list_of_dict)
+    num_words = get_num_words(text)
+    num_characters = count_characters(text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        ch = item["char"]
+        cnt = item["num"]
+        if ch.isalpha():
+            print(f"{ch}: {cnt}")
+    print("============= END ===============")
 
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
+
 
     
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,17 @@
+#stats.py
+
+#returns the number of words in a string
 def get_num_words(book_text):
     list_of_words = book_text.split()
     return len(list_of_words)
 
+#reads in a book and returns the text as a string
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+#counts the number of characters in a book and returns a dictionary with the character as the key and the number of times it appears as the value 
 def count_characters(book_text):
     character_count_dict = {}
     for i in book_text.lower():
@@ -11,5 +21,19 @@ def count_characters(book_text):
             character_count_dict[i] += 1
 
     return character_count_dict
-        
-        
+
+#takes a dictionary and returns a list of dictionaries
+def make_list_of_dictionaries(dictionary):
+    list_of_dict=[]
+    for i in (dictionary):
+        list_of_dict.append({"char": i,"num": dictionary[i]})
+    return list_of_dict
+
+def sort_helper(list_of_dict):
+    return list_of_dict["num"]
+
+#takes a list of dictionaries and sorts it by the value in descending order
+def sort_list_of_dictionaries(list_of_dict):
+    list_of_dict.sort(key=sort_helper, reverse=True)
+    return list_of_dict
+
